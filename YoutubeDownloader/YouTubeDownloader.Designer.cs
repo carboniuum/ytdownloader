81f6ff12826e3484d890b0888d5afd399b270c41
Umarbek <umarbek@mail.com>
2022-05-13 18:18:12 +0500

    commit

---
+++ YoutubeDownloader/YouTubeDownloader.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace YoutubeDownloader
+{
+    partial class YouTubeDownloader
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UrlTB = new System.Windows.Forms.TextBox();
+            this.SearchBtn = new System.Windows.Forms.Button();
+            this.ThumbnailPB = new System.Windows.Forms.PictureBox();
+            this.DownloadBtn = new System.Windows.Forms.Button();
+            this.QualityCB = new System.Windows.Forms.ComboBox();
+            this.ProgressBar = new System.Windows.Forms.ProgressBar();
+            this.ProgressLabel = new System.Windows.Forms.Label();
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.AuthorLabel = new System.Windows.Forms.Label();
+            this.DurationLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ThumbnailPB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // UrlTB
+            // 
+            this.UrlTB.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.UrlTB.Location = new System.Drawing.Point(13, 13);
+            this.UrlTB.Multiline = true;
+            this.UrlTB.Name = "UrlTB";
+            this.UrlTB.Size = new System.Drawing.Size(1239, 33);
+            this.UrlTB.TabIndex = 0;
+            // 
+            // SearchBtn
+            // 
+            this.SearchBtn.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SearchBtn.Location = new System.Drawing.Point(13, 53);
+            this.SearchBtn.Name = "SearchBtn";
+            this.SearchBtn.Size = new System.Drawing.Size(150, 35);
+            this.SearchBtn.TabIndex = 1;
+            this.SearchBtn.Text = "Search";
+            this.SearchBtn.UseVisualStyleBackColor = true;
+            this.SearchBtn.Click += new System.EventHandler(this.SearchBtn_Click);
+            // 
+            // ThumbnailPB
+            // 
+            this.ThumbnailPB.Location = new System.Drawing.Point(13, 95);
+            this.ThumbnailPB.Name = "ThumbnailPB";
+            this.ThumbnailPB.Size = new System.Drawing.Size(800, 450);
+            this.ThumbnailPB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ThumbnailPB.TabIndex = 2;
+            this.ThumbnailPB.TabStop = false;
+            this.ThumbnailPB.Visible = false;
+            // 
+            // DownloadBtn
+            // 
+            this.DownloadBtn.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DownloadBtn.Location = new System.Drawing.Point(169, 53);
+            this.DownloadBtn.Name = "DownloadBtn";
+            this.DownloadBtn.Size = new System.Drawing.Size(150, 35);
+            this.DownloadBtn.TabIndex = 3;
+            this.DownloadBtn.Text = "Download";
+            this.DownloadBtn.UseVisualStyleBackColor = true;
+            this.DownloadBtn.Visible = false;
+            this.DownloadBtn.Click += new System.EventHandler(this.DownloadBtn_Click);
+            // 
+            // QualityCB
+            // 
+            this.QualityCB.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.QualityCB.FormattingEnabled = true;
+            this.QualityCB.Location = new System.Drawing.Point(819, 186);
+            this.QualityCB.Name = "QualityCB";
+            this.QualityCB.Size = new System.Drawing.Size(148, 33);
+            this.QualityCB.TabIndex = 4;
+            this.QualityCB.Visible = false;
+            // 
+            // ProgressBar
+            // 
+            this.ProgressBar.Location = new System.Drawing.Point(13, 636);
+            this.ProgressBar.Name = "ProgressBar";
+            this.ProgressBar.Size = new System.Drawing.Size(1239, 33);
+            this.ProgressBar.Step = 1;
+            this.ProgressBar.TabIndex = 5;
+            // 
+            // ProgressLabel
+            // 
+            this.ProgressLabel.AutoSize = true;
+            this.ProgressLabel.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ProgressLabel.Location = new System.Drawing.Point(12, 608);
+            this.ProgressLabel.Name = "ProgressLabel";
+            this.ProgressLabel.Size = new System.Drawing.Size(0, 25);
+            this.ProgressLabel.TabIndex = 6;
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TitleLabel.Location = new System.Drawing.Point(12, 557);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(0, 25);
+            this.TitleLabel.TabIndex = 7;
+            // 
+            // AuthorLabel
+            // 
+            this.AuthorLabel.AutoSize = true;
+            this.AuthorLabel.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.AuthorLabel.Location = new System.Drawing.Point(819, 95);
+            this.AuthorLabel.Name = "AuthorLabel";
+            this.AuthorLabel.Size = new System.Drawing.Size(0, 25);
+            this.AuthorLabel.TabIndex = 8;
+            // 
+            // DurationLabel
+            // 
+            this.DurationLabel.AutoSize = true;
+            this.DurationLabel.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DurationLabel.Location = new System.Drawing.Point(819, 138);
+            this.DurationLabel.Name = "DurationLabel";
+            this.DurationLabel.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.DurationLabel.Size = new System.Drawing.Size(0, 25);
+            this.DurationLabel.TabIndex = 9;
+            // 
+            // YouTubeDownloader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.DurationLabel);
+            this.Controls.Add(this.AuthorLabel);
+            this.Controls.Add(this.TitleLabel);
+            this.Controls.Add(this.ProgressLabel);
+            this.Controls.Add(this.ProgressBar);
+            this.Controls.Add(this.QualityCB);
+            this.Controls.Add(this.DownloadBtn);
+            this.Controls.Add(this.ThumbnailPB);
+            this.Controls.Add(this.SearchBtn);
+            this.Controls.Add(this.UrlTB);
+            this.MaximizeBox = false;
+            this.Name = "YouTubeDownloader";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "YouTubeDownloader";
+            ((System.ComponentModel.ISupportInitialize)(this.ThumbnailPB)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox UrlTB;
+        private System.Windows.Forms.Button SearchBtn;
+        private System.Windows.Forms.PictureBox ThumbnailPB;
+        private System.Windows.Forms.Button DownloadBtn;
+        private System.Windows.Forms.ComboBox QualityCB;
+        private System.Windows.Forms.ProgressBar ProgressBar;
+        private System.Windows.Forms.Label ProgressLabel;
+        private System.Windows.Forms.Label TitleLabel;
+        private System.Windows.Forms.Label AuthorLabel;
+        private System.Windows.Forms.Label DurationLabel;
+    }
+}
+
---
+++ YoutubeDownloader/YouTubeDownloader.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YoutubeExplode;
+using YoutubeExplode.Common;
+using YoutubeExplode.Converter;
+using YoutubeExplode.Videos.Streams;
+
+namespace YoutubeDownloader
+{
+    public partial class YouTubeDownloader : Form
+    {
+        private readonly YoutubeClient _youtubeClient;
+        private StreamManifest _manifest;
+        private Video _data;
+
+        private readonly string _basePath = AppDomain.CurrentDomain.BaseDirectory;
+        private readonly string _thumbNailInput = "input.webp";
+        private readonly string _thumbNailOutput = "output.png";
+        private readonly string _fileName = $"video{Guid.NewGuid()}.mp4";
+
+        public YouTubeDownloader()
+        {
+            _youtubeClient = new YoutubeClient();
+
+            InitializeComponent();
+
+            if (File.Exists(_basePath + _thumbNailInput))
+            {
+                File.Delete(_basePath + _thumbNailInput);
+            }
+
+            if (File.Exists(_basePath + _thumbNailOutput))
+            {
+                File.Delete(_basePath + _thumbNailOutput);
+            }
+        }
+
+        private async void SearchBtn_Click(object sender, EventArgs e)
+        {
+            _data = await GetVideoData(UrlTB.Text.Trim());
+
+            if (_data != null)
+            {
+                ThumbnailPB.Visible = true;
+                DownloadBtn.Visible = true;
+                QualityCB.Visible = true;
+
+                AuthorLabel.Text = _data.Author;
+                DurationLabel.Text = _data.Duration;
+                TitleLabel.Text = _data.Title;
+
+                string ext = _data.Thumbnail.Split('.').LastOrDefault();
+
+                if (ext == "webp")
+                {
+                    using (var wc = new WebClient())
+                    {
+                        wc.DownloadFile(_data.Thumbnail, _thumbNailInput);
+                    }
+
+                    await Task.Run(() =>
+                    {
+                        var converter = new GroupDocs.Conversion.Converter(_basePath + _thumbNailInput);
+                        var convertOptions = converter.GetPossibleConversions()["png"].ConvertOptions;
+                        converter.Convert(_thumbNailOutput, convertOptions);
+                    });
+
+                    ThumbnailPB.Image = Image.FromFile(_basePath + _thumbNailOutput);
+                }
+                else
+                {
+                    ThumbnailPB.Load(_data.Thumbnail);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Something went wrong! Check your Internet connection");
+            }
+        }
+
+        private async void DownloadBtn_Click(object sender, EventArgs e)
+        {
+            await Download();
+        }
+
+        private async Task<Video> GetVideoData(string url)
+        {
+            try
+            {
+                var video = await _youtubeClient.Videos.GetAsync(url);
+                _manifest = await _youtubeClient.Videos.Streams.GetManifestAsync(url);
+
+                QualityCB.Items.AddRange(_manifest.GetVideoStreams().Select(s => s.VideoQuality.Label).Distinct().ToArray());
+                QualityCB.SelectedItem = QualityCB.Items[0];
+
+                return new Video
+                {
+                    Title = video.Title,
+                    Author = video.Author.ChannelTitle,
+                    Duration = video.Duration.ToString(),
+                    Url = video.Url,
+                    Thumbnail = video.Thumbnails.GetWithHighestResolution().Url
+                };
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error occured: {ex.Message}");
+            }
+
+            return null;
+        }
+
+        private async Task Download()
+        {
+            try
+            {
+                string folderPath = null;
+
+                using (var fbd = new FolderBrowserDialog())
+                {
+                    var result = fbd.ShowDialog();
+
+                    if (result == DialogResult.OK && !String.IsNullOrWhiteSpace(fbd.SelectedPath))
+                    {
+                        folderPath = fbd.SelectedPath;
+                    }
+                }
+
+                string path = $@"{folderPath}\{_fileName}" ?? _fileName;
+
+                var audioStreamInfo = _manifest.GetAudioStreams().GetWithHighestBitrate();
+                var videoStreamInfo = _manifest.GetVideoStreams().First(s => s.VideoQuality.Label == QualityCB.SelectedItem.ToString());
+                var streamInfos = new IStreamInfo[] { audioStreamInfo, videoStreamInfo };
+
+                await _youtubeClient.Videos.DownloadAsync(
+                    streamInfos,
+                    new ConversionRequestBuilder(path).Build(),
+                    new Progress<double>(p => 
+                    {
+                        ProgressBar.Value = (int)(p * 100);
+                        ProgressLabel.Text = (int)(p * 100) + "%";
+                    })
+                );
+                
+                MessageBox.Show("Download complete");
+                Reset();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error occured: {ex.Message}");
+            }
+        }
+
+        private void Reset()
+        {
+            ThumbnailPB.Visible = DownloadBtn.Visible = QualityCB.Visible = false;
+
+            AuthorLabel.Text = DurationLabel.Text = TitleLabel.Text = UrlTB.Text = ProgressLabel.Text = String.Empty;
+
+            ProgressBar.Value = 0;
+        }
+    }
+}
